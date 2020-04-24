cefca7741bf3cd4044bd94156226cb6adf9f279a
Haci Uzdil <uzdilhaci@gmail.com>
2020-04-24 13:33:20 -0500

    MyPractice1.java

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -3,5 +3,8 @@ package helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("merhaba dunya");
+        System.out.println("hello world");
+        System.out.println("Hello World");
+
     }
 }
---
+++ src/helloworld/MyPractice1.java
@@ -0,0 +1,8 @@
+package helloworld;
+
+public class MyPractice1 {
+    public static void main(String[] args) {
+        System.out.println(" My Practice page");
+
+    }
+}
